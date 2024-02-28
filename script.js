93115b73f0c22ffc2541c1642a16a015f0b16bbd
James Au <21522019@gm.uit.edu.vn>
2024-02-28 13:02:37 +0700

    fix: Add first list item

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
+
+function addTask() {
+    const task = inputBox.value.trim();
+    if (!task) {
+        alert("Please write down a task");
+        return;
+    }
+
+    const li = document.createElement("li");
+
+    li.innerHTML = `
+    <label>
+        <input type="checkbox" />
+        <span>${task}</span>
+        </label>
+    <span class="edit-btn">Edit</span>
+    <span class="delete-btn">Delete</span>
+    `;
+
+    listContainer.appendChild(li);
+    inputBox.value = "";
+}
